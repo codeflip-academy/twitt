101a1cc0962ebcc366c8f37602edd6d3fb275405
unknown <plwieseler@gmail.com>
2020-02-27 20:42:30 -0600

    Added Foreign Key References to the Profile and Post in the Reaction Table

--- database/Reaction.sql
+++ database/Reaction.sql
@@ -4,7 +4,11 @@ GO
 
 CREATE TABLE [Reaction]
 (
-	[State] BIT NOT NULL
+	[State] BIT NOT NULL,
+	[Profile] INT,
+	[Post] INT,
+	 FOREIGN KEY ([Profile]) REFERENCES [Profile](ID),
+	 FOREIGN KEY ([Post]) REFERENCES [Post](ID)
 )
 
 GO
