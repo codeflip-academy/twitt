c1c2c27ebc0c2da21e1386fc561989eef25b0b2c
Daniel Figueroa <Blackspade123@hotmail.com>
2020-02-28 20:34:45 -0600

    Added a quick deploy for the database

---
+++ database/deploy.ps1
@@ -0,0 +1,6 @@
+Invoke-Sqlcmd -InputFile DropDatabase.sql
+Invoke-Sqlcmd -InputFile Twitt.sql
+Invoke-Sqlcmd -InputFile Profile.sql
+Invoke-Sqlcmd -InputFile Post.sql
+Invoke-Sqlcmd -InputFile Comment.sql
+Invoke-Sqlcmd -InputFile Reaction.sql
